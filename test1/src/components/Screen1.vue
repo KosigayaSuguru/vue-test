807fc0df5eea68d14ce3fffa6f181e6fb6db633b
self2 <self2>
2017-11-21 02:56:40 +0900

    外部APIから取得したjsonを表示するサンプルのtmp

--- test1/src/components/Screen1.vue
+++ test1/src/components/Screen1.vue
@@ -1,15 +1,38 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <div v-html='sokuhou_json'></div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'Screen1',
   data () {
     return {
-      msg: 'routing Screen1'
+      msg: 'routing Screen1',
+      data: ''
+    }
+  },
+  created: function () {
+    this.test()
+  },
+  computed: {
+    sokuhou_json () {
+      if (this.data === '') {
+        return '<span>loding</span>'
+      } else {
+        return this.data.data.query.results.item
+      }
+    }
+  },
+  methods: {
+    test () {
+      axios.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%3D'http%3A%2F%2Fkancolle.doorblog.jp%2Findex.rdf'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys", {}).then((res) => {
+        this.data = res
+      })
     }
   }
 }
